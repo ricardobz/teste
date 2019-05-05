e62e28ba901a6a396afafd3b3430c7c2d1f3a5d1
root <root@RB-NOTE.localdomain>
2019-05-05 11:52:34 -0300

    fizzbuzz 3.0

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,19 +5,22 @@ Regras do jogo Fizzbuzz
 3. Se a posição for múltipla de 3 e 5: fizzbuzz
 4. Para todos os outros dizer o número
 """
+from functools import partial
 
-def multiple_of(base, num):
-    return num % base ==0
+
+multiple_of = lambda base, num : num % base == 0
+multiple_of_5 = partial (multiple_of, 5)
+multiple_of_3 = partial (multiple_of, 3)
 
 
 def robot(pos):
     say = str(pos)
 
-    if multiple_of(3, pos) and multiple_of(5, pos):
+    if multiple_of_5(pos) and multiple_of_3(pos):
         say = 'fizzbuzz'
-    elif multiple_of(5, pos):
+    elif multiple_of_5(pos):
         say = 'buzz'
-    elif multiple_of(3, pos):
+    elif multiple_of_3( pos):
         say = 'fizz'
 
     return say
